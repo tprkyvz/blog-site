f40007a6b38a5cfa3e9ef94db2fd69378c096bd3
Toprak Yavuz <toprakyvz0309@hotmail.com>
2026-04-21 01:28:20 +0300

    url fix

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -18,10 +18,10 @@ export default function Header() {
                     <Link href="/contact" className="linkicon" title="İletişim">
                         <FaEnvelope className="linkiconImage" />
                     </Link>
-                    <a href="https://github.com" target="_blank" className="linkicon" title="GitHub">
+                    <a href="https://github.com/tprkyvz" target="_blank" className="linkicon" title="GitHub">
                         <FaGithub className="linkiconImage" />
                     </a>
-                    <a href="https://linkedin.com" target="_blank" className="linkicon" title="LinkedIn">
+                    <a href="https://linkedin.com/in/toprak-yavuz-b15b60247/" target="_blank" className="linkicon" title="LinkedIn">
                         <FaLinkedin className="linkiconImage" />
                     </a>
                 </div>
